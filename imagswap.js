3ae8e34534452aa43832cd2a73f5428940149ad1
Dhanush-BioMedicalEngineer <dhanush8086@gmail.com>
2024-07-11 15:39:43 +0530

    Add files via upload

---
+++ imagswap.js
@@ -0,0 +1,14 @@
+const imageSources = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg'];
+function changeBackgroundImage() {
+const randomIndex = Math.floor(Math.random() * imageSources.length);
+document.body.style.backgroundImage = `url(${imageSources[randomIndex]})`;
+}
+// Change the background image every 1 seconds (1000 milliseconds)
+setInterval(changeBackgroundImage, 1000);
+
+
+
+
+for(let i=0; i<=10; i++){
+console.log(Math.random()*10);
+}
